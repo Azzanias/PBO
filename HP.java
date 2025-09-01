47999526b4659a828a94c7b73bd04a32d61831e2
Azzanias <azzanias814@gmail.com>
2025-09-01 23:24:18 +0700

    Add files via upload

---
+++ HP.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo;
+
+/**
+ *
+ * @author DIAZ
+ */
+public class HP {
+    
+    public void hidupkan() {
+        System.out.println("Handphone dihidupkan...");
+    }
+    
+    public void lakukanPanggilan() {
+        System.out.println("kring kring kring Memanggil...");
+    }
+    
+    public void kirimSMS() {
+        System.out.println("dung dung dung mengirim SMS...");
+    }
+    
+    public void Matikan() {
+        System.out.println("Hanphone dimatikan...");
+    }
+}
